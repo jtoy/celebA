e1d6d44890bfe59f7bd25f18da46ef9720f42ca5
Jason Toy <jasontoy@gmail.com>
2016-03-21 17:00:04 +0100

    initial

---
+++ collect.sh
@@ -0,0 +1,3 @@
+wget --no-check-certificate https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AADVdnYbokd7TXhpvfWLL3sga/img_align_celeba.zip?dl=1
+unzip img_align_celeba.zip\?dl\=1 
+find img_align_celeba -name '*.jpg'  -exec sh -c 'mv "$@" "$0"' ./ {} +
