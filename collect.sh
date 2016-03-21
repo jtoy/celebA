17762ae32cd899e80e6f68a1024f041aea1c52c4
jtoy <jtoy@jtoy.net>
2016-03-21 17:02:24 +0100

    Update collect.sh

--- collect.sh
+++ collect.sh
@@ -1,3 +1,4 @@
 wget --no-check-certificate https://www.dropbox.com/sh/8oqt9vytwxb3s4r/AADVdnYbokd7TXhpvfWLL3sga/img_align_celeba.zip?dl=1
 unzip img_align_celeba.zip\?dl\=1 
 find img_align_celeba -name '*.jpg'  -exec sh -c 'mv "$@" "$0"' ./ {} +
+rm -rf img_align_celeba
